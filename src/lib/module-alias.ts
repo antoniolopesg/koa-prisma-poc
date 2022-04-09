412dce6c02246b6a46d226b4b5a267461287f579
Antonio Lopes <antoniolopesg@protonmail.com>
2022-04-09 00:24:23 -0300

    add ts-node-dev

--- src/lib/module-alias.ts
+++ src/lib/module-alias.ts
@@ -1,4 +1,4 @@
 import { addAlias } from 'module-alias'
 import { resolve } from 'node:path'
 
-addAlias('@', resolve('dist'))
+addAlias('@', resolve(process.env.TS_NODE_DEV !== undefined ? 'src' : 'dist'))
