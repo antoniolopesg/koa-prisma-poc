481aaca6cda54b21e3241d1b6eb04a9b5d0a6fc4
Antonio Lopes <antoniolopesg@protonmail.com>
2022-04-08 22:51:30 -0300

    add koa

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import http from 'http'
+import app from './app'
+import { config } from './config'
+
+async function main (): Promise<void> {
+  const server = http.createServer(app.callback())
+
+  server.listen(config.PORT, () => {
+    console.log(`server listening on: http://localhost:${config.PORT}`)
+  })
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main()
