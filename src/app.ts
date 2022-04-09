2b37a0352acf494e75d6a5b843a9c43d3faff284
Antonio Lopes <antoniolopesg@protonmail.com>
2022-04-09 01:17:11 -0300

    add koa router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,39 @@
 import Koa from 'koa'
 import koaBody from 'koa-body'
+import Router from '@koa/router'
 
 const app = new Koa()
 
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.status = 500
+    ctx.body = {
+      message: 'Unknown Error'
+    }
+  }
+})
+
+const guestRouter = new Router()
+
 app.use(koaBody({ json: true }))
+
+// Open Routes
+guestRouter.get('/', ctx => {
+  ctx.status = 200
+  ctx.body = {
+    message: 'Hello Whats Grupos :)'
+  }
+})
+
+app.use(guestRouter.routes())
+
+app.use(ctx => {
+  ctx.body = {
+    message: 'Not found'
+  }
+  ctx.status = 404
+})
 
 export default app
