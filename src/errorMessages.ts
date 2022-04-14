d36cef122a00570ce623dcc0404a713707e44870
Antonio Lopes <antoniolopesg@protonmail.com>
2022-04-13 21:07:44 -0300

    add error messages file

---
+++ src/errorMessages.ts
@@ -0,0 +1,3 @@
+export const ONLY_AUTHENTICATED_USERS_CAN_ACESS_ERROR = 'Only authenticated users can access this route'
+
+export const INCORRECT_AUTHORIZATION_HEADER_FORMAT = 'Incorrect authorization header format'
